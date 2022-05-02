b17591fe36b7bcaec267d511620a1a5641b2715c
Ayan Banerjee <ayanbanerjee102@gmail.com>
2022-05-02 16:26:03 +0530

    Completed the api portion and also created the cardComponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import GlobalStyles, { Container } from "./globalStyles";
 import styled from "styled-components";
 import Pokemon from "./Pokemon";
+import PokemonService from "./services/pokemons";
+import { GenericButton } from "./GenericButton/GenericButton";
 
 const Title = styled.h1`
   text-align: center;
@@ -18,19 +21,52 @@ const AppContainer = styled(Container)`
 
 const PokemonsContainer = styled.div`
   display: flex;
-  flex-direction: column;
+  /* flex-direction: column; */
+  flex-wrap: wrap;
   justify-content: center;
   align-items: center;
+  margin: 1.3rem 0;
 `;
 
 function App() {
+  const [allPokemons, setAllPokemons] = useState([]);
+
+  useEffect(() => {
+    getAllPokemons();
+  }, []);
+
+  const getAllPokemons = async () => {
+    const p1 = new PokemonService(20);
+    let p2 = await p1.getAllPokemons();
+    p2.forEach(async (item, idx, arr) => {
+      let { data } = await p1.getPokemonDetails(item.url);
+      arr[idx] = data;
+      setAllPokemons(arr);
+    });
+
+    console.log(p2);
+  };
+
   return (
     <AppContainer>
       <GlobalStyles />
       <Title>Pokemon Codex</Title>
       <PokemonsContainer>
-        <Pokemon />
+        {allPokemons.length &&
+          allPokemons.map((pokemon, index) => {
+            console.log(pokemon.types);
+            return (
+              <Pokemon
+                id={pokemon.id}
+                name={pokemon.name}
+                type={pokemon.types[0].type.name}
+                image={pokemon.sprites.other.dream_world.front_default}
+                key={index}
+              />
+            );
+          })}
       </PokemonsContainer>
+      <GenericButton width={"20%"}>Load More</GenericButton>
     </AppContainer>
   );
 }
--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,22 +1,62 @@
-import React, { useEffect, useState } from "react";
-import PokemonService from "./services/pokemons";
+import React from "react";
+import styled from "styled-components";
 
-const Pokemon = () => {
-  useEffect(() => {
-    getAllPokemons();
-  }, []);
+const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 250px;
+  width: 170px;
+  margin: 0.5rem;
+  background-color: ${({ type }) => (type ? type : "white")};
+  border: 1px solid #e6e4e4;
+  border-radius: 0.5rem;
+  text-align: center;
+  box-shadow: 10px 10px 17px -11px rgba(0, 0, 0, 0.75);
+  cursor: pointer;
 
-  const getAllPokemons = async () => {
-    const p1 = new PokemonService(20);
-    let p2 = await p1.getAllPokemons();
-    p2.forEach(async (item, idx, arr) => {
-      let { data } = await p1.getPokemonDetails(item.url);
-      arr[idx] = data;
-    });
-    console.log(p2);
-  };
+  &:hover {
+    transform: scale(1.1);
+    transition: 0.4s all ease-in-out;
+  }
+`;
 
-  return <></>;
+const PokemonID = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  width: 100%;
+  margin: 3px 0;
+`;
+
+const ImageContainer = styled.img`
+  height: 120px;
+  width: 120px;
+`;
+
+const DetailWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+`;
+
+const Pokemon = ({ id, name, type, image }) => {
+  return (
+    <>
+      <CardContainer className={type}>
+        <PokemonID>
+          <small>${id}</small>
+        </PokemonID>
+        <ImageContainer src={image} />
+        <DetailWrapper>
+          <h3>{name}</h3>
+          <small>Type: {type}</small>
+        </DetailWrapper>
+      </CardContainer>
+    </>
+  );
 };
 
 export default Pokemon;
